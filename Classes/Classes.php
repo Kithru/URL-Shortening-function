8bb74c9e10a5cb893d3b283444d683807c003e74
Kithru Viduranga <86905325+Kithru@users.noreply.github.com>
2024-09-27 13:39:28 +0530

    Classes

---
+++ Classes/Classes.php
@@ -0,0 +1,43 @@
+<?php
+require_once "../Classes/DBConnect.php";
+
+class Classmanager {
+
+    public function geturldetails() {
+        $con = DBConnect::getConnection();
+        $sql = "Select * from `urls` ";
+        $result = mysql_query($sql, $con) or die(mysql_error());
+        $i=0;
+        $url = array();
+        while ($row = mysql_fetch_assoc($result)) {
+             $url[$i]['id'] = $row["id"];
+             $url[$i]['url'] = $row["url"]; 
+             $url[$i]['clicks'] = $row["clicks"];
+             $i++;
+        } 
+        mysql_close($con);
+        return $url;
+    } 
+    public function getshorturl($id) {
+        $con = DBConnect::getConnection();
+        $date = date("Y-m-d");
+        $sql = "Select clicks,url from `urls` where id ='$id'";
+        $results = mysql_query($sql, $con) or die(mysql_error());
+        $row = mysql_fetch_array($results, MYSQL_BOTH);
+        $clickcount = $row['clicks'];
+        $URL = $row['url'];
+        $clicks = $clickcount + 1 ;
+
+        $query = "update urls set clicks='$clicks' where id ='$id'";
+        $results = mysql_query($query, $con) or die(mysql_error());
+
+        if ($results) {
+            return $URL;
+        }
+    } 
+
+   
+}
+
+
+?>
